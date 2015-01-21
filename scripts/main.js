21fb8c2af9263f924d25acbd67c946ea09c917f5
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-21 15:43:20 -0500

    subtraction works! @mfees @jrutishauser

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,15 @@ console.log(add(1, 2))
 
 assert.equal(add(1, 2), 3)
 
+function diff(A, B) {
+    
+    return A - B;
+
+}
+
+console.log(diff(10, 3))
+
+assert.equal(diff(6, 3), 3)
+
+assert.equal(diff(7, 3), 4)
+
