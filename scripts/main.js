259a991e8e556a21f4a21e61eb4b1e7f3ea50e9b
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-22 17:37:00 -0500

    first move made, punk

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,11 @@ var board = [
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ];
-console.log(board.join('\n'))
+console.log(board.join('\n') + '\n\n');
+
+// Move King's Pawn forward 2
+
+board[4][4] = board[6][4];
+board[6][4] = ' ';
+
+console.log(board.join('\n'));
