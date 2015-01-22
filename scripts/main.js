4c1ebfc82a633f663c6b04c796f85aceebabfedf
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-22 17:52:16 -0500

    deleted the old way to print board and move.  added function to print board

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,12 @@ var board = [
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ];
-console.log(board.join('\n') + '\n\n');
 
 // Move King's Pawn forward 2
 
-board[4][4] = board[6][4];
-board[6][4] = ' ';
+var printBoard = function () {
+    console.log(board.join('\n') + '\n\n');
+};
 
-console.log(board.join('\n'));
+printBoard(); // printBoard is a function that takes no arguments since it just prints a board from the array
+
