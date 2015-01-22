f43312a6295f41f1bd0256958d423d1632c0e054
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-22 02:34:55 -0500

    cleaning up, tired, who knows

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,13 @@ function add(A, B) {
     return A + B;
 }
 console.log(add(1, 2));
-
 assert.equal(add(1, 2), 3);
-
 function diff(A, B) {
-    
     return A - B;
-
 }
 
 console.log(diff(10, 3));
-
 assert.equal(diff(6, 3), 3);
-
 assert.equal(diff(7, 3), 4);
 
 function prod(A, B) {
@@ -24,19 +18,14 @@ function prod(A, B) {
 }
 
 console.log(prod(6, 7));
-
 assert.equal(prod(3, 2), 6);
-
 assert.equal(prod(10, 3), 30);
-
 
 function div(A, B) {
     return A / B;
-    
-}
+    }
 
 console.log(div(60, 10));
-
 assert.equal(div(10, 2), 5);
 assert.equal(div(20, 5), 4);
 
