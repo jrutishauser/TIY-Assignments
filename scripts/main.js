63f5020c48592cad038e3663cd4612c4cd81cacd
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-22 22:38:17 -0500

    added the first 8 moves of the catalan opening to the chess board all functions working fine

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,6 @@ var board = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ];
 
-// Move King's Pawn forward 2
 
 var printBoard = function () {
     console.log(board.join('\n') + '\n\n');
@@ -17,3 +16,20 @@ var printBoard = function () {
 
 printBoard(); // printBoard is a function that takes no arguments since it just prints a board from the array
 
+var moveMe = function (fromX, fromY, toX, toY) {
+    fromX = fromX - 1;
+    fromY = fromY - 1;
+    toX = toX - 1;
+    toY = toY - 1;
+    board[toY][toX] = board[fromY][fromX];
+    board[fromY][fromX] = ' ';
+    printBoard();
+};
+moveMe(4, 7, 4, 5);
+moveMe(7, 1, 6, 3);
+moveMe(3, 7, 3, 5);
+moveMe(5, 2, 5, 3);
+moveMe(7, 7, 7, 6);
+moveMe(4, 2, 4, 4);
+moveMe(6, 8, 7, 7);
+moveMe(6, 1, 5, 2);
