b24e5f7a99aec47e08a6a56c40dfb5a330dfee1d
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-27 18:17:50 -0500

    gymnastics updates with class walktthrough

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,53 +1,37 @@
 var events = require('../apis/github/users/jrutishauser/events.json');
 var assert = require('assert');
 
-
-
-
-
-function answer() {
-    return {
-        'total': events.length,
-        'PushEvent': {
-            'total': 0,
-            'perDay': 0,
-            
+function answer () {
+    var pushEvents = events.filter(function(item){
+        return item.type == 'PushEvent';
+    });
+    var days = events.filter(function(item){
+        return item.type == 'updated_at';
+    });
+    return { 
+    'total': events.length,
+    'PushEvent': {
+        'total': pushEvents.length,
         }
     };
 }
 console.log(answer());
-
-var index = 0;
-var numberOfPushEvents = 0;
-
-var PushEvents = events.filter(function(item) {
-    return item..type == 'PushEvent';
-});
-
-
-
-
-
-describe('the setup', function () {
+console.log(events.length);
+var theAnswer = answer();
 
 it('should have events', function () {
-assert(events);
+    assert(events);
 });
-it('should have an `answer` function', function () {
-assert(answer);
-assert(typeof answer == 'function');
-});
-});
-
-describe('the answer',  function () {
-    var theAnswer = answer();
-    
-it('should have 30 total events', function () {
-assert(theAnswer.total === 30);
+it('should have a function answer', function () {
+   assert(answer);
 });
 
 
-it('should have PushEvents entries', function () {
-assert(theAnswer.PushEvents.total);
+it('should return the length of events', function () {
+    assert.equal(theAnswer.total, 30);
 });
+
+it('should contain PushEvent and has a total count', function () {
+   assert(theAnswer.PushEvent);
+    assert(theAnswer.PushEvent.total);
 });
