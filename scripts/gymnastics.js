d7ae090e6130c91a3d6f730dccbaeabd58132a0d
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-27 09:51:22 -0500

    doing some gymnastics

---
+++ scripts/gymnastics.js
@@ -0,0 +1,40 @@
+var events = require('../apis/github/users/jrutishauser/events.json');
+var assert = require('assert');
+
+
+
+
+
+function answer() {
+    return {
+        'total': events.length
+    }
+}
+console.log(answer());
+
+
+
+describe('the setup', function() {
+
+it('should have events', function () {
+assert(events);
+});
+it('should have an `answer` function', function () {
+assert(answer);
+assert(typeof answer == 'function');
+});
+
+});
+
+describe('the answer',  function () {
+    var theAnswer = answer();
+    
+it('should have 30 total events', function () {
+assert(theAnswer.total === 30);
+});
+
+
+it('should have PushEvents entries', function () {
+assert(theAnswer.PushEvents.total);
+});
+});
