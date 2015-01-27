c80763ac2b32ea57bf80eefa1918b5cef9a764cc
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-26 23:06:23 -0500

    created two four loops with an array, trying to figure out my errors and get this thing working

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -1,21 +1,23 @@
-var assert = require('assert');
 
 var stringDict = [ ["zero", 0], ['one', 1], ['two', 2], ['three', 3], ['four', 4], ['five', 5], ['six', 6], ['seven', 7], ['eight', 8], ['nine', 9], ['ten', 10]
 ]
 
 
-for ( var i=0; i <= 10; i++ ) {
-
-    for ( var j=0; j <= 10; j++ ) {
-    function plus(A, B){
-    return i + j;
+function plus(A, B){
+    for (var i = 0; i <= stringDict.length; i++ ) {
+        if (A == stringDict[i][0] ) {
+            var firstNumber = stringDict[i][1];
+            break;
         }
-        it('should add ' + stringDict[i][0] + ' and ' + stringDict[j][0], function(){
-        assert.equal(plus(stringDict[i][0], stringDict[j][0]), i + j);
-        });
- 
-    } // close j loop
-} // close i loop
-
-//  121)  should add ten and ten:
- //    TypeError: Cannot read property '0' of undefined -- @@@@ Why am I getting this error???!
+    }
+    for (var j = 0; j <= stringDict.length; j++ ) {
+        if (A == stringdict[j][0] ) {
+            var secondNumber = stringDict[j][1];
+            break;
+        }
+    }
+    return firstNumber + secondNumber;
+    console.log(firstNumber);
+    console.log(secondNumber);
+console.log(plus ("one", "three"));
+}
