4778e254df3c7cf4cdf86b9052d52b06e4997e1f
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-27 22:30:01 -0500

    modified my string calculator from before, trying to take it to the hundreds without having to rewrite any more arrays.  it works-ish, not perfect

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -4,8 +4,19 @@ var toEnglish = function (number) {
   number = number.toString();
   var newNumber = "";
   for ( var i = 0; i < number.length; i++ ) {
-    newNumber += datArray[number[i]];
-  }
-  return newNumber;
+    newNumber += datArray[number[i]] + " ";
+  } 
+    if (newNumber.length > 2) {
+        var converted = newNumber.split(" ");
+        converted.pop();
+        converted.splice(-2,0, "hundred");
+        var convertedAgain = converted.join(" ");  
+    } 
+return convertedAgain;
 }
-console.log(toEnglish(1234));
+console.log(toEnglish(304));
+
+//trying to play with taking this further without having to extend the array much.
+// ran into an issue with going for the thousands since I cant do another if
+// found "switch" which may work but not sure, will have to revisit this later.
+
