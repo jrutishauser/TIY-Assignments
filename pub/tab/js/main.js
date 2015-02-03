c818ae7d3884de909a64876c50c5cffd24c75ae8
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-03 02:35:16 -0500

    probably not the best way to do this but at least Im getting some typeing practice in. setting all the IDs from the HTML and CSS page to variables.  planning on having the onclick function hide all the sections except for the one clicked

--- pub/tab/js/main.js
+++ pub/tab/js/main.js
@@ -1,1 +1,33 @@
 
+
+
+
+
+
+
+var one = document.getElementById('firstSec');
+var two = document.getElementById('secSec');
+var three = document.getElementById('thirdSec');
+var four = document.getElementById('fourthSec');
+var five = document.getElementById('fifthSec');
+
+var lOne = document.getElementById('pageOne');
+var lTwo = document.getElementById('pageTwo');
+var lThree = document.getElementById('pageThree');
+var lFour = document.getElementById('pageFour');
+var lFive = document.getElementById('pageFive');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
