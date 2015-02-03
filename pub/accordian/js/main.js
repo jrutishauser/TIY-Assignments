ade703fab2434a6c1b97ee2c10f3f5762593847e
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-02 23:58:00 -0500

    first attempt at having the tabs underneath close.  unnsuccessful.  turning in failed attempts ftw

--- pub/accordian/js/main.js
+++ pub/accordian/js/main.js
@@ -2,26 +2,25 @@
 
 
 var clickToShow = function () {
+	if (this.nextElementSibling.className === 'open'){
+		this.nextElementSibling.remove('open');
+	} else {
+
 	this.nextElementSibling.className = 'open';
-
-
-} 
-
-
-
-
+	}
+}; 
 
 var articleHeadings = document.getElementsByTagName('h3');
 
 for (var index = 0; index < articleHeadings.length; index++){
 	articleHeadings[index].onclick = clickToShow;
-
-
 }
 
+var subArticleHeadings = document.getElementsByTagName('h4');
 
-
-
+for (var index2 = 0; index2 < subArticleHeadings.length; index2++){
+	subArticleHeadings[index2].onclick = clickToShow;
+}
 
 
 
