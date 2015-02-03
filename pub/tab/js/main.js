3f257f1bc11ccc6b12d4f35c6fc562d310c9c6d1
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-03 10:10:37 -0500

    all tabs change content, not really tabs

--- pub/tab/js/main.js
+++ pub/tab/js/main.js
@@ -17,6 +17,29 @@ var secFunc = function () {
 	five.className = 'hide';
 };
 
+var thirdFunc = function () {
+	one.className = 'hide';
+	two.className = 'hide';
+	three.className = 'show';
+	four.className = 'hide';
+	five.className = 'hide';
+};
+
+var fourthFunc = function () {
+	one.className = 'hide';
+	two.className = 'hide';
+	three.className = 'hide';
+	four.className = 'show';
+	five.className = 'hide';
+};
+
+var fifthFunc = function () {
+	one.className = 'hide';
+	two.className = 'hide';
+	three.className = 'hide';
+	four.className = 'hide';
+	five.className = 'show';
+};
 
 var one = document.getElementById('firstSec');
 var two = document.getElementById('secSec');
@@ -32,9 +55,9 @@ var lFive = document.getElementById('pageFive');
 
 lTwo.onclick = firstFunc;
 lOne.onclick = secFunc; 
-
-
-
+lThree.onclick = thirdFunc;
+lFour.onclick = fourthFunc;
+lFive.onclick = fifthFunc;
 
 
 
