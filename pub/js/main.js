a1835002d5c9e23ec200f46fa0694ff3967ca8ea
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-07 14:38:50 -0500

    playing with new features

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,7 +2,16 @@
 	'use strict';
 
     $(document).ready(function(){
-	jQuery.when(
+
+   $.getJSON('../apis/github/users/jrutishauser.json', function(json) {
+   	console.log(json.login);
+   }); 
+
+
+
+
+
+    jQuery.when(
 		jQuery.getJSON('../apis/github/users/jrutishauser.json')
 	).done(function(json) {
 	console.log(json.login);
