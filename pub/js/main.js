10dca5c3315fb0982b38ef99727f9a246be6f50a
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-07 13:05:09 -0500

    what the heck

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,38 +1,8 @@
+(function (window) {
+	'use strict';
+    var user = $.get('../apis/github/users/eklemen/eklemen.json');
 
+    $(document).ready(function(){
+    })
 
-
-var repLink = document.getElementById('repl');
-var conLink = document.getElementById('conl');
-var pubLink = document.getElementById('publ');
-
-var repCon = document.getElementById('rep');
-var pubCon = document.getElementById('pub');
-var conCon = document.getElementById('con');
-
-var repFunc = function () {
-	repCon.className = 'show';
-	pubCon.className = 'hide';
-	conCon.className = 'hide';
-};
-
-var pubFunc = function () {
-	repCon.className = 'hide';
-	pubCon.className = 'show';
-	conCon.className = 'hide';
-};
-
-
-var conFunc = function () {
-	repCon.className = 'hide';
-	pubCon.className = 'hide';
-	conCon.className = 'show';
-};
-
-repLink.onclick = repFunc;
-conLink.onclick = conFunc;
-pubLink.onclick = pubFunc;
-
-
-
-
-
+})(window);
