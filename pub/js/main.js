4fee7853d2bbc9d45e88c632cd0a124588198454
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-07 22:54:43 -0500

    back to repos

--- pub/js/main.js
+++ pub/js/main.js
@@ -22,7 +22,7 @@
 		
 	    var repoItems = $.map(repos, function(name, i){
 	    	var listItem = $('<li></li>');
-		$('<href="#"><h3>' + repos[i].name + '</h3></a>').appendTo(listItem);
+		$('<a href="#"><h3>' + repos[i].name + '</h3></a>').appendTo(listItem);
 
 			return listItem;
 	    
