96a6c08bf7694190040fb0490270cbeaccf9df7e
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-09 18:11:09 -0500

    time displays playing with background color

--- pub/colorclock/js/main.js
+++ pub/colorclock/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+	
+	var jsTimer = function () {
+		var t = new Date();
+		return $('#theTime').html(t.toTimeString().slice(0, 8));
+	};
+	var sliceMe = setInterval(function(){jsTimer();}, 1000);
 
+	console.log(jsTimer());
+
+
+});
