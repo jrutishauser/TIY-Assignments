8c47d907d202b3b702f6ab38236517a6fd6816b8
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-10 22:52:19 -0500

    last

--- pub/NotLChessboard/js/main.js
+++ pub/NotLChessboard/js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
 
+	var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+	
+	
+	
+	function Piece(color, name, position){
+		this._color = color;
+		this._name = name;
+		this._position = position;
+	}
+
+	var whiteKing = new Piece('white', 'king');
+	var whitePawn = new Piece('white', 'pawn');
+	var whiteRook = new Piece('white', 'rook');
+
+
+
+
+
+});
