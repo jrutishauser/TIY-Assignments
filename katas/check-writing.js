916b24205f83f86f037b68eca59a294bcbe215ec
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-13 22:57:44 -0500

    added the numbers past 20 that seem necessary like, thirty, fifty, up to hundred and also thousand and million.  not sure these will be needed the same way but wrote them out so I can visually think about what I am doing

--- katas/check-writing.js
+++ katas/check-writing.js
@@ -18,9 +18,18 @@ var integerDict = {
 	17 : 'seventeen',
 	18 : 'eighteen',
 	19 : 'nineteen',
-	20 : 'twenty'
+	20 : 'twenty',
+	30 : 'thirty',
+	40 : 'fourty',
+	50 : 'fifty',
+	60 : 'sixt',
+	70 : 'seventy',
+	80 : 'eighty',
+	90 : 'ninety',
+	100 : 'hundred',
+	1000 : 'thousand',
+	1000000 : 'million'
 };
-
 var integToStr = function (integer) {
 	for (var ints in integerDict) {
 		if (integer == ints) {
@@ -28,3 +37,10 @@ var integToStr = function (integer) {
 	}	}
 };
 integToStr(5);
+
+
+
+
+
+
+
