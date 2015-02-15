57a1ddaf195c500f0e3d40cf7133e5a2fb8569f8
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-15 17:32:17 -0500

    push event totals added.  also created function to filter array for each item

--- api-gymnastics/main.js
+++ api-gymnastics/main.js
@@ -1,4 +1,12 @@
 var events = require('./events.json');
+var _ = require('lodash');
+
+
+var eventSearch = function (eventType) {
+	return _.filter(events, {'type': eventType});
+};
+
+
 
 
 var answer = function () {
@@ -6,8 +14,8 @@ console.log(events.length);
 	return {
 		'total': events.length,
 		'PushEvent': {
-			'total': 10,
-			'perDay': 2,	
+			'total': eventSearch('PushEvent').length,
+			'perDay': 2	
 		}	
 	
 	
@@ -15,8 +23,7 @@ console.log(events.length);
 	};
 
 
-
 };
 
-
-answer();
+console.log(eventSearch('PushEvent'));
+console.log(answer());
