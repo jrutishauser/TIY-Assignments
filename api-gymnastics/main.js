5b14cc8a4b936d493671455264725b58dee59c89
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-15 16:38:16 -0500

    writing out data to pull

--- api-gymnastics/main.js
+++ api-gymnastics/main.js
@@ -3,7 +3,16 @@ var events = require('./events.json');
 
 var answer = function () {
 console.log(events.length);
-
+	return {
+		'total': events.length,
+		'PushEvent': {
+			'total': 10,
+			'perDay': 2,	
+		}	
+	
+	
+	
+	};
 
 
 
