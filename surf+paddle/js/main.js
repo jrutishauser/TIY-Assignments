86497092c1c3682fb5f67b8366eaa7aa52871cfd
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-16 00:50:27 -0500

    turns out the jquery for mouse enter and mouseleave and also hover did not work as I expected.  interesting.

--- surf+paddle/js/main.js
+++ surf+paddle/js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function () {
 
+$('.blue').mouseover(
+	function() {
+		$(this).css('display', 'none');});
+	
+
+$('.blue').mouseleave(
+	function() {
+		$(this).css('display', 'inline');});
+
+
+});
